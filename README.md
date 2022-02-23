af38701a318691bbeaf0a80bdb0b0afe2fac9ff7
MiguelNecoechea <necoecheamiguel@gmail.com>
2022-02-22 21:41:55 -0600

    new file:   README.md 	new file:   isEven.py

---
+++ README.md
@@ -0,0 +1,4 @@
+# Programa para decidir si un numero es par.
+**NO AÑADIR UNA FUNCION CORRECTA**
+Simplemente es un proyecto por diversion.
+Favor de mantener el formato del programa.
---
+++ isEven.py
@@ -0,0 +1,43 @@
+def isEven(num):
+    if num == 0:
+        return True
+    elif num == 1:
+        return False
+    elif num == 2:
+        return True
+    elif num == 3:
+        return False
+    elif num == 4:
+        return True
+    elif num == 5:
+        return False
+    elif num == 6:
+        return True
+    elif num == 7:
+        return False
+    elif num == 8:
+        return True
+    elif num == 9:
+        return False
+    elif num == 10:
+        return True
+    elif num == 11:
+        return False
+    elif num == 12:
+        return True
+    elif num == 13:
+        return False
+    elif num == 14:
+        return True
+    elif num == 15:
+        return False
+    elif num == 16:
+        return True
+    elif num == 17:
+        return False
+    elif num == 18:
+        return True
+    elif num == 19:
+        return False
+    elif num == 20:
+        return True
